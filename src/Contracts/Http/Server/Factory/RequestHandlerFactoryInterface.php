1622c3317fbaeaaeff3a379c37e7aa465fd1d262
Julien Duseyau <julien.duseyau@gmail.com>
2025-10-25 12:14:34 +0200

    Add HTTP message interfaces and factories for request handling

---
+++ src/Contracts/Http/Server/Factory/RequestHandlerFactoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Maduser\Argon\Contracts\Http\Server\Factory;
+
+use Maduser\Argon\Middleware\Contracts\RequestHandlerFactoryInterface as MiddlewareRequestHandlerFactoryInterface;
+use Maduser\Argon\Container\Exceptions\ContainerException;
+use Maduser\Argon\Container\Exceptions\NotFoundException;
+use Psr\Http\Server\RequestHandlerInterface;
+
+interface RequestHandlerFactoryInterface extends MiddlewareRequestHandlerFactoryInterface
+{
+    /**
+     * @throws NotFoundException
+     * @throws ContainerException
+     * @psalm-suppress PossiblyUnusedReturnValue
+     */
+    public function create(string $cacheKey = 'http_pipeline'): RequestHandlerInterface;
+}
---
+++ src/Provider/ArgonMessageServiceProvider.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Maduser\Argon\Http\Provider;
+
+use Maduser\Argon\Container\AbstractServiceProvider;
+use Maduser\Argon\Container\ArgonContainer;
+use Maduser\Argon\Container\Exceptions\ContainerException;
+use Maduser\Argon\Http\Message\Factory\ResponseFactory;
+use Maduser\Argon\Http\Message\Factory\ServerRequestFactory;
+use Maduser\Argon\Http\Message\Factory\StreamFactory;
+use Maduser\Argon\Http\Message\Factory\UploadedFileFactory;
+use Maduser\Argon\Http\Message\Factory\UriFactory;
+use Maduser\Argon\Http\Message\Response;
+use Maduser\Argon\Http\Message\ServerRequest;
+use Maduser\Argon\Http\Message\Stream;
+use Maduser\Argon\Http\Message\UploadedFile;
+use Maduser\Argon\Http\Message\Uri;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+class ArgonMessageServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * @throws ContainerException
+     */
+    public function register(ArgonContainer $container): void
+    {
+        $container->set(ServerRequestFactoryInterface::class, ServerRequestFactory::class)
+            ->tag(['http', 'psr-17']);
+
+        $container->set(ServerRequestInterface::class, ServerRequest::class)
+            ->factory(ServerRequestFactoryInterface::class)
+            ->tag(['http', 'psr-7'])
+            ->transient();
+
+        $container->set(ResponseFactoryInterface::class, ResponseFactory::class)
+            ->tag(['http', 'psr-17']);
+
+        $container->set(ResponseInterface::class, Response::class)
+            ->factory(ResponseFactoryInterface::class, 'createResponse')
+            ->tag(['http', 'psr-7'])
+            ->transient();
+
+        $container->set(StreamFactoryInterface::class, StreamFactory::class)
+            ->tag(['http', 'psr-17']);
+
+        $container->set(StreamInterface::class, Stream::class)
+            ->factory(StreamFactoryInterface::class, 'createStream')
+            ->tag(['http', 'psr-7'])
+            ->transient();
+
+        $container->set(UriFactoryInterface::class, UriFactory::class)
+            ->tag(['http', 'psr-17']);
+
+        $container->set(UriInterface::class, Uri::class)
+            ->factory(UriFactoryInterface::class, 'createUri')
+            ->tag(['http', 'psr-7'])
+            ->transient();
+
+        $container->set(UploadedFileFactoryInterface::class, UploadedFileFactory::class)
+            ->tag(['http', 'psr-17']);
+
+        $container->set(UploadedFileInterface::class, UploadedFile::class)
+            ->factory(UploadedFileFactoryInterface::class, 'createUploadedFile')
+            ->tag(['http', 'psr-7'])
+            ->transient();
+    }
+}
